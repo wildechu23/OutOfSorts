8a2097993643c28bd3a070571fc09e51571285ad
Wilde Chu <wildechu23@gmail.com>
2020-12-18 10:50:24 -0500

    slightly better tester

--- Tester.java
+++ Tester.java
@@ -2,15 +2,11 @@ import java.util.Random;
 import java.util.Arrays;
 public class Tester {
   public static void main(String[] args) {
-    Random rng = new Random(100);// seed of 100 is stored.
-    for (int i = 0; i < 5; i++) {
-      System.out.println(rng.nextInt() % 1000);
-    }
+    int arraySize = 20;
+    int[] data = new int[arraySize];
+    Random rng = new Random(200);// seed of 100 is stored.
 
-    int[] data = new int[5];
-
-    rng = new Random(100);// seed of 100 is stored.
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < arraySize; i++) {
       data[i] = rng.nextInt() % 1000;
     }
 
